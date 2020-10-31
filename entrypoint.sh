786c97ffe1de9157006e93233c72ab8e03b1b2bd
Fabiano Parente <fabiano@nunes.me>
2020-10-31 18:13:22 -0300

    ordena argumentos

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ repo="ratelimitpreview/test"
 url_token="https://auth.docker.io/token?service=registry.docker.io&scope=repository:${repo}:pull"
 url_pull="https://registry-1.docker.io/v2/${repo}/manifests/latest"
 
-bearer_token=$(curl ${auth:+--user "$auth"} --silent "${url_token}" | jq --raw-output .token)
+bearer_token=$(curl --silent ${auth:+--user "$auth"} "${url_token}" | jq --raw-output .token)
 auth_header="Authorization: Bearer ${bearer_token}"
 
 curl --silent --verbose --header "${auth_header}" "${url_pull}" 2>&1 | grep -E '< (HTTP|RateLimit)'
