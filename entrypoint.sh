1ffd691ff090c63efe79e0412282b2261ce5b76d
Fabiano Parente <fabiano@nunes.me>
2021-06-14 10:31:09 -0300

    grep insensível à caixa, tendo em vista que os headers passaram a ser em minúsculas

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ url_pull="https://registry-1.docker.io/v2/${repo}/manifests/latest"
 bearer_token=$(curl --silent ${auth:+--user "$auth"} "${url_token}" | jq --raw-output .token)
 auth_header="Authorization: Bearer ${bearer_token}"
 
-curl --head --silent --verbose --header "${auth_header}" "${url_pull}" 2>&1 | grep -E '< (HTTP|RateLimit)'
+curl --head --silent --verbose --header "${auth_header}" "${url_pull}" 2>&1 | grep -Ei '< (HTTP|RateLimit)'
